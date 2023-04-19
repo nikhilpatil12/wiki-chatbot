6c8088ed4cc8569b4f08d9b4bf5e457ac5cb5381
nikhilpatil12 <patil.nikhil12@outlook.com>
2023-04-18 22:12:09 -0700

    add login, signup forms; managing login in db-API

--- chatbot-ui/src/app/user-login/user-login.component.ts
+++ chatbot-ui/src/app/user-login/user-login.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent {
+  login = () => {
 
+  }
+  signup = () => {
+
+  }
 }
